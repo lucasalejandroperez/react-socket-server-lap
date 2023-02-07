8b0bc9369a980030dd66ce7b500bdbeebc619908
Lucas Perez <lucas.alejandro.perez@gmail.com>
2023-02-07 11:41:46 -0300

    Se agrega la parte de CORS bien

--- models/server.js
+++ models/server.js
@@ -15,7 +15,10 @@ class Server {
         this.server = http.createServer(this.app);
 
         // Configuraciones de sockets
-        this.io = socketio(this.server, { /* configuraciones */});
+        this.io = socketio(this.server, { cors: {
+            origin: "*",
+            methods: ["GET", "POST"]
+        }});
     }
 
     middlewares() {
